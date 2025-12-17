5855a7c2e0e187a4d8d7bb1a86d2366b17869daf
siddharthX6174 <siddharth_x6174@outlook.com>
2025-12-17 19:14:40 +0530

    add data in a table route

--- 10_DataBase/route.py
+++ 10_DataBase/route.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask("__main__")
@@ -13,6 +13,35 @@ else:
     conn.execute('CREATE TABLE if not exists students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
     print("Table created successfully")
 conn.close()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/addStudent')
+def add_student():
+    return render_template('addStudent.html')
+
+@app.route('/saveStudent', methods=['POST', 'GET'])
+def save_student():
+    if request.method == 'POST':
+        try:
+            name = request.form.get('studname')
+            addr = request.form.get('studaddr')
+            city = request.form.get('studcity')
+            pin = request.form.get('studpin')
+            with sqlite3.connect('mycollege.db') as conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO students (name, addr, city, pin) VALUES (?, ?, ?, ?)", (name, addr, city, pin))
+                conn.commit()
+
+                msg = "Record inserted successfully"
+        except:
+            conn.rollback()
+            msg = "Could not insert Data"
+
+    return render_template('success.html', msg=msg)
 
 if __name__ == "__main__":
     app.run(debug=True)
